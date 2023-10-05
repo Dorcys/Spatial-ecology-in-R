7d07d09ef84e14381c28c4c38ec5d63dee8fc585
Dorcys <146853862+Dorcys@users.noreply.github.com>
2023-10-05 18:57:21 +0300

    Update 01.POP.R

--- code /01.POP.R
+++ code /01.POP.R
@@ -1,1 +1,18 @@
+Tree <- 10
+#package need for pattern analyssis
+install.packages("spatstat")
 
+library(spatstat)
+bei
+plot(bei)
+plot(bei, cex=1, pch=20)
+bei.extra
+plot(bei.extra)
+
+# lets use only part of the dataset: elev
+plot(bei.extra$elev)
+e11 <- plot(bei.extra$elev)
+e11
+plot(e11)
+bei.extra[1]
+plot(bei.extra[1])
