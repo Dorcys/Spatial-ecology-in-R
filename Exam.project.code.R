87ed028ec0ec2aa5dcaf962e8c8c1c61efebd124
Dmitrii Ivanov <146853862+Dorcys@users.noreply.github.com>
2025-02-13 23:55:45 +0100

    Update Exam.project.code.R
    0_0

--- Exam.project.code.R
+++ Exam.project.code.R
@@ -8,9 +8,14 @@
 #Downloading packages
 install.packages("imageRy")
 install.packages("terra")
+install.packages("viridis")
+install.packages("ggplot2")
+
 #To use them, we need type additional command
-library(imageRy)
+library(imageRy) 
 library(terra)
+library(viridis) 
+library(ggplot2) 
 
 ###3.Analysis
 #Downloading pictures
@@ -42,45 +47,35 @@ plot(c_inB2024)
 plot(c_mB2020)
 plot(c_mB2024)
 #Note: Use for cleaning plots dev.off()
-#Stacking images for further analysis
-stackedIN <- c(c_inB2020[[1:3]], c_inB2024[[1:3]])
-stackedM <- c(c_mB2020[[1:3]], c_mB2024[[1:3]])
-plot(stackedM)
 
-#Calculation of NDVI
-#Ploting RGB version of our images
-im.plotRGB(c_inB2020, r=3,g=1,b=2)
-im.plotRGB(c_inB2024, r=3,g=1,b=2)
-dev.off()
-#Plot the NIR band 
+#Calculation of DVI & NDVI
+#Plot NIR band to see representation of vegetation  
 plot(c_inB2020[[1]])
+plot(c_mB2020[[1]])
+
 #Calculate the Difference Vegetation Index (DVI)
 inDVI0 <- c_inB2020[[1]] - c_inB2020[[2]]
 inDVI4<- c_inB2024[[1]] - c_inB2024[[2]]
-par(mfrow = c (2,1))
-plot(inDVI0)
-plot(inDVI4)
+mDVI0 <- c_mB2020[[1]] - c_mB2020[[2]]
+mDVI4 <- c_mB2024[[1]] - c_mB2024[[2]]
+
 # Calculate the Normalized Difference Vegetation Index (NDVI)
 inNDVI0 <- (c_inB2020[[1]] - c_inB2020[[2]]) / (c_inB2020[[1]] + c_inB2020[[2]])  
 inNDVI4 <- (c_inB2024[[1]] - c_inB2024[[2]]) / (c_inB2024[[1]] + c_inB2024[[2]]) 
 mNDVI0 <- (c_mB2020[[1]] - c_mB2020[[2]]) / (c_mB2020[[1]] + c_mB2020[[2]])
 mNDVI4 <- (c_mB2024[[1]] - c_mB2024[[2]]) / (c_mB2024[[1]] + c_mB2024[[2]])
 
-par(mfrow = c (2,2))
-plot(inNDVI0)
-plot(inNDVI4)
-plot(mNDVI0)
-plot(mNDVI4)
-dev.off()
+#Change in DVI 
+DVI_diff_IN <- inDVI4 - inDVI0
+DVI_diff_M <- mDVI4 - mDVI0
+summary(values(DVI_diff_IN))
+summary(values(DVI_diff_M))
+
 #Change in NDVI
 NDVI_diff_IN <- inNDVI4 - inNDVI0
 NDVI_diff_M <- mNDVI4 - mNDVI0
-plot(NDVI_diff_IN, main="NDVI Change - Indigenous Land")
-plot(NDVI_diff_M, main="NDVI Change - Modern Land")
-
 summary(values(NDVI_diff_IN))
 summary(values(NDVI_diff_M))
-
 t.test(values(NDVI_diff_IN), values(NDVI_diff_M))
 
 #Perform PCA 
@@ -89,25 +84,62 @@ inB4_PCA <- im.pca2(c_inB2024)
 mB0_PCA <- im.pca2(c_mB2020)
 mB4_PCA <- im.pca2(c_mB2024)
 
-#Extract 1 PCA
+#Extract First PCA as most important
 ipc10 <- inB0_PCA$PC1
 ipc14 <- inB4_PCA$PC1
 mpc10 <- mB0_PCA$PC1
 moc14 <- mB4_PCA$PC1
 
-#Changes 
-pc1_diff_IN <- ipc14 - ipc10  # Change in Indigenous land
-pc1_diff_M <- moc14 - mpc10    # Change in Modern land
+#Changes of cover in different type of land
+# Change in Indigenous land
+pc1_diff_IN <- ipc14 - ipc10
+# Change in Modern land
+pc1_diff_M <- moc14 - mpc10
 pc1_diff_IN
 pc1_diff_M
-
-#Visualization 
-par(mfrow=c(1,2))  
-plot(pc1_diff_IN, main="PCA Difference - Indigenous Land")  
-plot(pc1_diff_M, main="PCA Difference - Modern Land")  
 
 #Quantifying Forest Loss
 forest_loss_IN <- sum(pc1_diff_IN[] < 0, na.rm=TRUE) / ncell(pc1_diff_IN) * 100
 forest_loss_M <- sum(pc1_diff_M[] < 0, na.rm=TRUE) / ncell(pc1_diff_M) * 100
 print(paste("Forest loss in Indigenous land:", round(forest_loss_IN, 2), "%"))
 print(paste("Forest loss in Modern land:", round(forest_loss_M, 2), "%"))
+# Convert to hectares
+# Get resolution in square meters
+res_m2 <- res(NDVI_diff_IN)[1] * res(NDVI_diff_IN)[2]
+area_lost_IN <- sum(NDVI_diff_IN[] < 0, na.rm=TRUE) * res_m2 / 10000
+area_lost_M <- sum(NDVI_diff_M[] < 0, na.rm=TRUE) * res_m2 / 10000  
+print(paste("Forest loss in Indigenous land:", round(area_lost_IN, 2), "hectares"))
+print(paste("Forest loss in Modern land:", round(area_lost_M, 2), "hectares"))
+
+
+###4.Data visualization
+#This part of code dedicated for creating images for presentation 
+#DVI & NDVI
+
+plot(DVI_diff_IN, col=viridis(100), main="DVI Change - Indigenous Land")
+legend("bottomright", legend=c("Decrease", "No Change", "Increase"),
+       fill=viridis(3), bty="n")
+
+plot(DVI_diff_M, col=viridis(100), main="DVI Change - Modern land")
+legend("bottomright", legend=c("Decrease", "No Change", "Increase"),
+       fill=viridis(3), bty="n")
+
+plot(NDVI_diff_IN, col=viridis(100), main="NDVI Change - Indigenous Land")
+legend("bottomright", legend=c("Decrease", "No Change", "Increase"),
+       fill=viridis(3), bty="n")
+
+plot(NDVI_diff_M, col=viridis(100), main="NDVI Change - Modern land")
+legend("bottomright", legend=c("Decrease", "No Change", "Increase"),
+       fill=viridis(3), bty="n")
+#Histogram for NDVI 
+hist(values(NDVI_diff_IN), breaks=50, col=viridis(100), main="NDVI Change - Indigenous Land", xlab="NDVI Change")
+hist(values(NDVI_diff_M), breaks=50, col=viridis(100), main="NDVI Change - Modern Land", xlab="NDVI Change")
+
+#PCA
+plot(PCA_diff_IN, col=viridis(100), main="PCA Change - Indigenous Land")  
+legend("bottomright", legend=c("Decrease", "No Change", "Increase"),
+       fill=viridis(3), bty="n") 
+
+plot(PCA_diff_M, col=viridis(100), main="PCA Change - Indigenous Land")  
+legend("bottomright", legend=c("Decrease", "No Change", "Increase"),
+       fill=viridis(3), bty="n")
